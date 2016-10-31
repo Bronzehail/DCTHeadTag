630aa7e68b4895075b64cbcab7e0c86bef662f9d
DKQoo2 <stevenliu219@gmail.com>
2016-10-31 23:04:09 +0800

    賴打哇嗚嗚嗚
    OAO

--- src/HeadTag/HeadTag.php
+++ src/HeadTag/HeadTag.php
@@ -1,122 +1,50 @@
 <?php
+
 namespace HeadTag;
 
-use pocketmine\command\Command;
-use pocketmine\command\CommandSender;
-use pocketmine\event\block\BlockBreakEvent;
-use pocketmine\event\block\BlockPlaceEvent;
-use pocketmine\event\entity\EntityLevelChangeEvent;
-use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\event\player\PlayerMoveEvent;
-use pocketmine\event\player\PlayerQuitEvent;
-use pocketmine\event\player\PlayerToggleSneakEvent;
-use pocketmine\level\Level;
-use pocketmine\level\position;
-use pocketmine\math\Vector3;
-use pocketmine\Player;
-use pocketmine\plugin\Plugin;
 use pocketmine\plugin\PluginBase;
 use pocketmine\Server;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 use pocketmine\utils\Config;
+use pocketmine\Player;
+use pocketmine\scheduler\PluginTask;
 use pocketmine\utils\TextFormat;
-
 use onebone\economyapi\EconomyAPI;
+class HeadTag extends PluginBase{
 
-/*
-遊戲ID
-模式
-權限
-錢
-血量
-IP
-*/
-class HeadTag extends PluginBase implements Listener {
+    public function onEnable(){
+		$this->EconomyAPI = $this->getServer()->getPluginManager()->getPlugin("EconomyAPI");
+		if(!$this->EconomyAPI){
+			$this->getServer()->getPluginManager()->disablePlugin($this);
+			$this->getLogger()->error("您沒有安裝EconomyAPI");
+			return;
+		}
+		@mkdir($this->getDataFolder());
+		$this->getServer()->getScheduler()->scheduleRepeatingTask(new TimeUpdate($this), 10);
+    }
+}
+
+class TimeUpdate extends PluginTask {
+public function __construct(HeadTag $plugin)
+    {
+        parent::__construct($plugin);
+        $this->plugin = $plugin;
+    }
 	
-	public function onEnable() {
-		$this->getserver()->getPluginManager()->registerEvents($this,$this);
-			@mkdir($this->getDataFolder());
-				$this->join= new Config($this->getDataFolder()."Config.yml", Config::YAML, array("名子"=>"名子","模式"=>"模式","權限"=>"權限","錢"=>"錢","血量"=>"血量","IP"=>"IP","頭部顯示"=>"on"));
-					$this->getLogger()->info(TEXTFORMAT::GOLD . "§cDCT§e碼頭創造者 §6HeadTag 頭部顯示加載中");
-	}
-	public function onCommand(CommandSender $sender,Command $cmd,$label,array $args){
-	$n=$sender->getPlayer()->getName();
-	$p=$sender->getPlayer();
-	$vtag=$this->join->get("頭部");
-	switch(strtolower($cmd->getName())){
-		case "headtag":
-			if($headtag=="on"){
-				$this->join->set("頭部","off");
-					$this->join->save();
-			return true;
-      			   }else{
-      				if($headtag=="off"){
-       					$this->join->set("頭部","on");
-        					$this->join->save();
-         						$sender->sendmessage("§a成功開啟");
-         		return true;
-      }
-	public function join(PlayerJoinEvent $e){
-		$p=$e->getPlayer();
-			$gm=$p->getgamemode();
-				$ip=$p->getAddress();
-					$p1=$this->join->get("名字");
-						$p2=$this->join->get("模式");
-							$p3=$this->join->get("權限");
-								$p4=$this->join->get("錢");
-									$p5=$this->join->get("血量");
-										$p6=$this->join->get("IP");
-											$op=$p->isOp();
-		if($gm=="1"){
-			$x="§a創造";
-				}
-				if($gm=="0"){
-					$x="§c生存";
-						}
-						if($op){
-							$o="§a管理員";
-								}else{
-									$o="§6玩家";
-										}
-										$h=$p->getHealth();
-											$mh=$p->getMaxHealth();
-												$m=EconomyAPI::getInstance()->myMoney(strtolower($p->getName()));
-													}
-	public function move(PlayerMoveEvent $e){
-		$p=$e->getPlayer();
-			$gm=$p->getgamemode();
-				$ip=$p->getAddress();
-					$p1=$this->join->get("名字");
-						$p2=$this->join->get("模式");
-							$p3=$this->join->get("權限");
-								$p4=$this->join->get("錢");
-									$p5=$this->join->get("血量");
-										$p6=$this->join->get("IP");
-											$op=$p->isOp();
-		if($gm=="1"){
-			$x="§a創造";
-				}
-				if($gm=="0"){
-					$x="§c生存";
-						}
-						if($op){
-							$o="§a管理員";
-							}else{
-								$o="§6玩家";
-									}
-									$h=$p->getHealth();
-										$mh=$p->getMaxHealth();
-											$m=EconomyAPI::getInstance()->myMoney(strtolower($p->getName()));
-												$jj=$this->join->get("頭部");
-													if($this->join->get("頭部")=="on"){
-														$p->setNameTag("§a".$p1."§6".$p."\n§a".$p2.".".$x."\n§a".$p3.".".$o."\n§a".$p4."§6".$m."\n§a".$p5.".".$h."|".$mh."\n§a".$p6.". §c".$ip."");
-															}
-															if($this->join->get("頭部")=="off"){
-																}
-																	}
-	public function onDisable(){
-		$this->getLogger()->info(TEXTFORMAT::RED . "HeadTag 頭部顯示卸載");
-	}
-}	
-
-?>
+	public function onRun($currentTick){
+$result = array();
+		foreach($this->plugin->getServer()->getOnlinePlayers() as $player){
+switch($player->getGamemode()){
+				case 0: $gm = "生存";break;
+				case 1: $gm = "創造";break;
+				case 2: $gm = "冒險";break;
+				case 3: $gm = "觀看";break;
+			}
+$h = $player->getHealth();
+$m = $this->plugin->EconomyAPI->mymoney($player);
+			$name = $player->getName();
+			$player->setNameTag("交給賴打");
+			}
+}
+}
