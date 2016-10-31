7ca54cbbcef72483d254eb815e1f2d0e2a0b9153
DKQoo2 <stevenliu219@gmail.com>
2016-10-31 23:09:09 +0800

    Update HeadTag.php

--- src/HeadTag/HeadTag.php
+++ src/HeadTag/HeadTag.php
@@ -44,7 +44,7 @@ switch($player->getGamemode()){
 $h = $player->getHealth();
 $m = $this->plugin->EconomyAPI->mymoney($player);
 			$name = $player->getName();
-			$player->setNameTag("交給賴打");
+			$player->setNameTag("$name\n$m\n$h\n$gm");
 			}
 }
 }
