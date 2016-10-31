41c7a2b2afb964eea0c5448863fb12bb0a6593ec
TSRlightda <lightda104530@gmail.com>
2016-10-31 23:52:09 +0800

    Update HeadTag.php

--- src/HeadTag/HeadTag.php
+++ src/HeadTag/HeadTag.php
@@ -2,26 +2,33 @@
 
 namespace HeadTag;
 
-use pocketmine\plugin\PluginBase;
-use pocketmine\Server;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\utils\Config;
 use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
 use pocketmine\scheduler\PluginTask;
+use pocketmine\Server;
+use pocketmine\utils\Config;
 use pocketmine\utils\TextFormat;
+
 use onebone\economyapi\EconomyAPI;
+
 class HeadTag extends PluginBase{
 
     public function onEnable(){
-		$this->EconomyAPI = $this->getServer()->getPluginManager()->getPlugin("EconomyAPI");
-		if(!$this->EconomyAPI){
-			$this->getServer()->getPluginManager()->disablePlugin($this);
-			$this->getLogger()->error("您沒有安裝EconomyAPI");
-			return;
-		}
-		@mkdir($this->getDataFolder());
-		$this->getServer()->getScheduler()->scheduleRepeatingTask(new TimeUpdate($this), 10);
+	    $this->getServer()->getPluginManager()->registerEvents($this,$this);
+		$this->getServer()->getPluginManager()->getPlugin('EconomyAPI') == null){
+ 			$this->getLogger()->WARNING(TextFormat::RED ."您尚未安裝EconomyAPI！已經為您準備好兼容包！);
+ 				file_put_contents($this->getDataFolder() . "EAPI.phar", stream_get_contents($this->getResource("EAPI.phar")));//移动插件
+ 					$plugin = $this->getServer()->getPluginManager()->loadPlugin($this->getDataFolder() . "EAPI.phar");//加载插件
+ 						$this->getServer()->getPluginManager()->enablePlugin($plugin);
+ 							$this->getLogger()->NOTICE(TextFormat::GOLD ."兼容包已經安裝完畢！");
+								}else{
+  									$this->getLogger()->NOTICE(TextFormat::GOLD ."找到EconomyAPI！");
+										$this->getLogger()->info(TEXTFORMAT::GOLD . ">>  頭部顯示加載中")
+										}
+										@mkdir($this->getDataFolder());
+											$this->getServer()->getScheduler()->scheduleRepeatingTask(new TimeUpdate($this), 10);
     }
 }
 
