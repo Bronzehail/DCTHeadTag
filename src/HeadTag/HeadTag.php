561535da3dcbeb2ef8126cbc031ca1c5ca5c259a
TSRlightda <lightda104530@gmail.com>
2016-12-03 23:48:04 +0800

    On and Close was done :D

--- src/HeadTag/HeadTag.php
+++ src/HeadTag/HeadTag.php
@@ -2,26 +2,19 @@
 
 namespace HeadTag;
 
-use pocketmine\command\Command;
-use pocketmine\command\CommandSender;
-use pocketmine\Player;
-use pocketmine\plugin\PluginBase;
-use pocketmine\scheduler\PluginTask;
-use pocketmine\Server;
-use pocketmine\utils\Config;
-use pocketmine\utils\TextFormat;
-
-use FactionsPro\Main1;
 use HeadTag\TimeUpdate;
-use Love\love;
-use onebone\economyapi\EconomyAPI;
+use pocketmine\plugin\PluginBase;
 
 class HeadTag extends PluginBase{
 
-    public function onEnable(){
-	$this->getServer()->getPluginManager()->registerEvents($this,$this);
-		$this->getLogger()->info(TEXTFORMAT::GOLD . ">>  頭部顯示加載中")
-			$this->getServer()->getScheduler()->scheduleRepeatingTask(new TimeUpdate($this), 10);
-    }
+   public function onEnable(){
+	$this->getServer()->getLogger()->info(TEXTFORMAT::GOLD . ">> 頭部顯示加載!!");
+		$this->getServer()->getScheduler()->scheduleRepeatingTask(new TimeUpdate($this), 20);
+	}
+	
+	public function onDisable(){
+	    $this->getServer()->getLogger()->info(TEXTFORMAT::GOLD . ">> 頭部顯示卸載!!");
+	}
+
 }
 
