466a882d00f1f8614da0b0b865468a2c2e762849
TSRlightda <lightda104530@gmail.com>
2016-12-03 14:40:19 +0800

    Update HeadTag.php

--- src/HeadTag/HeadTag.php
+++ src/HeadTag/HeadTag.php
@@ -34,32 +34,3 @@ class HeadTag extends PluginBase{
     }
 }
 
-class TimeUpdate extends PluginTask {
-public function __construct(HeadTag $plugin)
-    {
-        parent::__construct($plugin);
-        $this->plugin = $plugin;
-    }
-	
-	public function onRun($currentTick){
-$result = array();
-		foreach($this->plugin->getServer()->getOnlinePlayers() as $player){
-switch($player->getGamemode()){
-				case 0: $gm = "生存模式";
-					break;
-					case 1: $gm = "創造模式";
-						break;
-						case 2: $gm = "冒險模式";
-							break;
-							case 3: $gm = "觀看模式";
-								break;
-								}
-								$h = $player->getHealth();
-									$m = $this->plugin->EconomyAPI->mymoney($player);
-										$mygh = Main1::getInstance()->getPlayerFaction($player->getName());
-											$marry = love::getInstance()->checklove($player->getName());
-												$name = $player->getName();
-													$player->setNameTag("$name \n $marry \n $mygh \n $m \n $h \n $gm ");
-			}
-}
-}
