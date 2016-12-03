5b18eba7b4cd78a3f4f965948a72909a226cc58e
TSRlightda <lightda104530@gmail.com>
2016-12-03 23:51:54 +0800

    TimeUpdate:D

--- src/HeadTag/TimeUpdate.php
+++ src/HeadTag/TimeUpdate.php
@@ -2,7 +2,15 @@
 
 namespace TimeUpdate;
 
+use pocketmine\Player;
 use pocketmine\scheduler\PluginTask;
+use pocketmine\Server;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+
+use FactionsPro\Main1;
+use Love\love;
+use onebone\economyapi\EconomyAPI;
 
 class TimeUpdate extends PluginTask {
 
@@ -13,23 +21,23 @@ public function __construct(HeadTag $plugin)
     }
 	
 	public function onRun($currentTick){
-    $result = array();
-		  foreach($this->plugin->getServer()->getOnlinePlayers() as $player){
-        switch($player->getGamemode()){
-				  case 0: $gm = "生存模式";
-					  break;
-					    case 1: $gm = "創造模式";
-						    break;
-						      case 2: $gm = "冒險模式";
-							      break;
-							        case 3: $gm = "觀看模式";
-								        break;
-								        }
-								        $h = $player->getHealth();
-									        $m = $this->plugin->EconomyAPI->mymoney($player);
-										        $mygh = Main1::getInstance()->getPlayerFaction($player->getName());
-										        	$marry = love::getInstance()->checklove($player->getName());
-											        	$name = $player->getName();
-												        	$player->setNameTag("$name \n $marry \n $mygh \n $m \n $h \n $gm ");
+    		$result = array();
+			foreach($this->plugin->getServer()->getOnlinePlayers() as $player){
+        			switch($player->getGamemode()){
+					case 0: $gm = "生存模式";
+					break;
+						case 1: $gm = "創造模式";
+						break;
+							case 2: $gm = "冒險模式";
+							break;
+								case 3: $gm = "觀看模式";
+								break;
+								}
+								$h = $player->getHealth();
+									$m = $this->plugin->EconomyAPI->mymoney($player);
+										$mygh = Main1::getInstance()->getPlayerFaction($player->getName());
+											$marry = love::getInstance()->checklove($player->getName());
+												$name = $player->getName();
+													$player->setNameTag("$name \n $marry \n $mygh \n $m \n $h \n $gm ");
 			}
 }
